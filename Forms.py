619a392e692ea6c25e120ec09c473d3d4a6d3fb5
Ryan Teo <ryanteoxuebin@gmail.com>
2021-12-19 15:42:29 +0800

    first version

---
+++ Forms.py
@@ -0,0 +1,15 @@
+from wtforms import Form, StringField, SelectField, TextAreaField, validators
+from wtforms.fields import EmailField, DateField
+
+class CreateUserForm(Form):
+    first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
+    last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
+    gender = SelectField('Gender', [validators.DataRequired()], choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')], default='')
+    
+class CreateCustomerForm(Form):
+    first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
+    last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
+    gender = SelectField('Gender', [validators.DataRequired()], choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')], default='')
+    email = EmailField('Email', [validators.Email(), validators.DataRequired()])
+    date_joined = DateField('Date Joined', format='%Y-%m-%d')
+    address = TextAreaField('Mailing Address', [validators.length(max=200), validators.DataRequired()])
---
+++ User.py
@@ -0,0 +1,33 @@
+class User:
+    count_id=0
+
+    def __init__(self, first_name, last_name, gender):
+        self.__class__.count_id+=1
+        self.__user_id=self.__class__.count_id
+        self.__first_name=first_name
+        self.__last_name=last_name
+        self.__gender=gender
+    
+    def get_user_id(self):
+        return self.__user_id
+      
+    def get_first_name(self):
+        return self.__first_name
+      
+    def get_last_name(self):
+        return self.__last_name
+      
+    def get_gender(self):
+        return self.__gender
+
+    def set_user_id(self, user_id):
+        self.__user_id = user_id
+    
+    def set_first_name(self, first_name):
+        self.__first_name = first_name
+
+    def set_last_name(self, last_name):
+        self.__last_name = last_name
+
+    def set_gender(self, gender):
+        self.__gender = gender
