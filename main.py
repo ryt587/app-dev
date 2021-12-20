5f70c7babadbc6f3061d81b156e885aa39e93485
ryt587 <76485067+ryt587@users.noreply.github.com>
2021-12-20 20:57:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/contactUs')
 def contact_us():
