5fa136e0b6edf19933758cc8bd057edcddc8e692
ryt587 <76485067+ryt587@users.noreply.github.com>
2021-12-21 14:21:43 +0800

    Update Forms.py

--- Forms.py
+++ Forms.py
@@ -2,14 +2,20 @@ from wtforms import Form, StringField, SelectField, TextAreaField, validators
 from wtforms.fields import EmailField, DateField
 
 class CreateUserForm(Form):
-    first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    gender = SelectField('Gender', [validators.DataRequired()], choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')], default='')
+    email = EmailField('Email', [validators.Email(), validators.DataRequired()])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
     
 class CreateCustomerForm(Form):
     first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
     last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    gender = SelectField('Gender', [validators.DataRequired()], choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')], default='')
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
     email = EmailField('Email', [validators.Email(), validators.DataRequired()])
-    date_joined = DateField('Date Joined', format='%Y-%m-%d')
     address = TextAreaField('Mailing Address', [validators.length(max=200), validators.DataRequired()])
