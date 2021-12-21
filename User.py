fd822b7b3dc331d77fcc4bfa067d1bf562702ea3
ryt587 <76485067+ryt587@users.noreply.github.com>
2021-12-21 14:10:27 +0800

    Update User.py

--- User.py
+++ User.py
@@ -1,21 +1,17 @@
 class User:
     count_id=0
 
-    def __init__(self, first_name, last_name, password):
+    def __init__(self, email, password):
         self.__class__.count_id+=1
         self.__user_id=self.__class__.count_id
-        self.__first_name=first_name
-        self.__last_name=last_name
+        self.__email=email
         self.__password=password
     
     def get_user_id(self):
         return self.__user_id
       
-    def get_first_name(self):
-        return self.__first_name
-      
-    def get_last_name(self):
-        return self.__last_name
+    def get_email(self):
+        return self.__email
       
     def get_password(self):
         return self.__password
@@ -23,11 +19,8 @@ class User:
     def set_user_id(self, user_id):
         self.__user_id = user_id
     
-    def set_first_name(self, first_name):
-        self.__first_name = first_name
-
-    def set_last_name(self, last_name):
-        self.__last_name = last_name
+    def set_email(self, email):
+        self.__email = email
 
     def set_password(self, password):
         self.__password = password
