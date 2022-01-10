32f99bf3d77e1eb37418e8d6952aa2f751b8f2ba
AVL0RD <89195479+AVL0RD@users.noreply.github.com>
2022-01-10 18:34:48 +0800

    Update Staff.py

--- Staff.py
+++ Staff.py
@@ -3,7 +3,7 @@ import User as u
 class Staff(u.User):
     
     def __init__(self, staff_id, email, password)
-        super().__innit__(email, password)
+        super().__init__(email, password)
         self.__staff_id = staff_id
         
     def get_staff_id(self):
