ac43c9aa74de9204ac37e55f3735230e47f75360
AVL0RD <89195479+AVL0RD@users.noreply.github.com>
2022-01-10 19:24:31 +0800

    Update Products.py

--- Products.py
+++ Products.py
@@ -1,4 +1,34 @@
 Class Products:
   
-  def __init__(        ):
+  def __init__(self, product_size, product_type, product_category, product_stock):
+      self.__product_size = product_size
+      self.__product_type = product_type
+      self.__product_category = product_category
+      self.__product_stock = product_stock
+      
+  def get_product_size(self):
+      return self.__product_size
+  
+  def get_product_type(self):
+       return self.__product_type
+  
+  def get_product_category(self):
+      return self.__category
+     
+  def get_product_stock(self):
+      return self.__product_stock
+    
+    
+    
+   def set_product_size(self, product_size):
+      self.__product_size = product_size
+      
+   def set_product_type(self, product_type):
+      self.__product_type = product_type
+      
+   def set_product_category(self, product_category):
+      self.__product_category = product_category
+      
+   def set_product_stock(self, product_stock):
+      self.__product_stock = product_stock
       
