52eae700abf16f4e7ba3e393ef2720413b3328a1
AVL0RD <89195479+AVL0RD@users.noreply.github.com>
2022-01-10 20:28:55 +0800

    Update Staff.py

--- Staff.py
+++ Staff.py
@@ -2,15 +2,22 @@ import User as u
 
 class Staff(u.User):
     
-    def __init__(self, staff_id, phone_number, email, password)
+    def __init__(self, staff_role phone_number, email, password)
         super().__init__(email, password)
-        self.__staff_id = staff_id
+        self.__staff_role = staff_role
         self.__phone_number = phone_number
-        
-    def get_staff_id(self):
-        return self.__staff_id
     
     def get_phone_number(self):
         return self.__phone_number
+    
+    def get_staff_role(self):
+        return self.__staff_role
+    
+    
+    def set_phone_number(self, phone_number):
+        self.__phone_number = phone_number
+        
+    def set_staff_role(self, staff_role):
+        self.__staff_role = staff_role
     
     
