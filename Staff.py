1bc7c8f05b46bb1bde62775aa4277118bc9e83e6
AVL0RD <89195479+AVL0RD@users.noreply.github.com>
2022-01-10 18:06:01 +0800

    Update Staff.py

--- Staff.py
+++ Staff.py
@@ -2,4 +2,9 @@ import User as u
 
 class Staff(u.User):
     
-    def __init__(
+    def __init__(self, staff_id, email, password)
+        super().__innit__(email, password)
+        self.__staff_id = staff_id
+        
+    def get_staff_id(self):
+        return self.__staff_id
