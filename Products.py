87b10570a69004e540ab38b5f5262b4fc18c37d8
ryt587 <ryanteoxuebin@gmail.com>
2022-01-11 11:35:38 +0800

    .

--- Products.py
+++ Products.py
@@ -1,10 +1,30 @@
+from uuid import uuid4
+import shelve
+
 class Product():
-  
+   id=uuid4().hex
+    
+   try:
+      db = shelve.open('user.db', 'r')
+      try:
+         users_dict = db['Users']
+         while id in users_dict:
+            id=uuid4().hex
+      except:
+         print("Error in retrieving Users from user.db.")
+      db.close()
+   except:
+      print("Error in retrieving Users from user.db.") 
+      
    def __init__(self, product_size, product_type, product_category, product_stock):
+      self.__product_id = Product.id
       self.__product_size = product_size
       self.__product_type = product_type
       self.__product_category = product_category
       self.__product_stock = product_stock
+   
+   def get_product_id(self):
+      return self.__product_id
       
    def get_product_size(self):
       return self.__product_size
@@ -18,7 +38,10 @@ class Product():
    def get_product_stock(self):
       return self.__product_stock
     
-    
+   
+   
+   def set_product_id(self, product_id):
+      self.__product_id = product_id
     
    def set_product_size(self, product_size):
       self.__product_size = product_size
