e95bc649dbb646f1de7a728bfdfbb8b4ca0fb140
AVL0RD <89195479+AVL0RD@users.noreply.github.com>
2022-01-13 16:32:27 +0800

    Update Products.py

--- Products.py
+++ Products.py
@@ -16,42 +16,27 @@ class Product():
    except:
       print("Error in retrieving Users from user.db.") 
       
-   def __init__(self, product_size, product_type, product_category, product_stock):
+   def __init__(self, product_stock, product_image):
       self.__product_id = Product.id
-      self.__product_size = product_size
-      self.__product_type = product_type
-      self.__product_category = product_category
       self.__product_stock = product_stock
+      self.__product_image = product_image
    
    def get_product_id(self):
       return self.__product_id
-      
-   def get_product_size(self):
-      return self.__product_size
-  
-   def get_product_type(self):
-      return self.__product_type
-  
-   def get_product_category(self):
-      return self.__product_category
      
    def get_product_stock(self):
       return self.__product_stock
     
+   def get_product_image(self):
+      return self.__product_image
    
    
    def set_product_id(self, product_id):
       self.__product_id = product_id
-    
-   def set_product_size(self, product_size):
-      self.__product_size = product_size
-      
-   def set_product_type(self, product_type):
-      self.__product_type = product_type
-      
-   def set_product_category(self, product_category):
-      self.__product_category = product_category
       
    def set_product_stock(self, product_stock):
       self.__product_stock = product_stock
+      
+   def set_product_image(self, product_image):
+      self.__product_image = product_image
       
