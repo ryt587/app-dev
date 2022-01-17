c333091aa1585cf4689920da97431b53536e6775
Kool35T <rbd.victory@gmail.com>
2022-01-17 19:18:11 +0800

    remove ]]

--- Forms.py
+++ Forms.py
@@ -71,4 +71,3 @@ class UpdateProductsForm(Form):
                              render_kw={"placeholder": "First Name"})
     Product_category = StringField('Product Category', [validators.Length(min=1, max=150), validators.DataRequired()],
                              render_kw={"placeholder": "Product Category"})
-]]
