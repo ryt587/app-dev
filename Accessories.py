9f354631e3421220646cf02a92f4fb67fa728613
AVL0RD <89195479+AVL0RD@users.noreply.github.com>
2022-02-12 17:15:01 +0800

    Update Accessories.py

--- Accessories.py
+++ Accessories.py
@@ -1,1 +1,20 @@
+import Products as p
 
+class Clothing(p.Product):
+    def __init__(self, name, product_stock, product_image, created_product, price, colour, size,):
+        super().__init__(name, product_stock, product_image, created_product, price)
+        self.__colour = colour
+        self.__size = size,
+
+    def get_colour(self):
+        return self.__colour
+
+    def get_size(self):
+        return self.__size
+    
+
+    def set_colour(self, colour):
+        self.__colour = colour
+
+    def set_size(self, size):
+        self.__size = 
